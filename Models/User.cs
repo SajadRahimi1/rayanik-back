567e4820d4c62cceeed7fdd9c3a80156fc10b7a6
Sajad <sajadrahimi0101@gmail.com>
2023-08-06 10:12:50 +0330

    added some prop to user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,7 @@
 public class UserRepository : BaseEntity
 {
-
+    public string? fullName { get; set; }
+    public string? phoneNumber { get; set; }
+    public string? email { get; set; }
+    public string? code { get; set; }
 }
